8de5fc4aec0621f837a206ed6e733980c7879e39
Joseph792 <joseph_camilo@ucne.edu.do>
2026-03-04 19:45:57 -0400

    Agregando CharacterResponseDto al proyecto

---
+++ app/src/main/java/edu/ucne/joseph_camilo_ap2_p2/domain/model/CharacterResponseDto.kt
@@ -0,0 +1,19 @@
+package edu.ucne.joseph_camilo_ap2_p2.domain.model
+
+data class CharacterResponseDto(
+    val items: List<CharacterDto>
+)
+data class CharacterDto(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val maxKi: String,
+) {
+    fun toDomain() = Character(
+        id, name, ki, race, gender, description, image, maxKi,
+    )
+}
